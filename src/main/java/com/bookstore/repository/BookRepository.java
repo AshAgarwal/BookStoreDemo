2ec3aa6319c5dba710d443e3c0425b224a95ad19
abhishekshah27 <adsh1135@gmail.com>
2020-09-10 09:43:39 +0530

    modified

--- src/main/java/com/bookstore/repository/BookRepository.java
+++ src/main/java/com/bookstore/repository/BookRepository.java
@@ -1,12 +1,9 @@
 package com.bookstore.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
 import com.bookstore.model.Book;
 
-public interface BookRepository extends JpaRepository<Book,Long> {
+public interface BookRepository extends CrudRepository<Book,Long> {
 
-//	@Modifying
-//	@Query("update Book b SET b.9
-//	void deactivateUsersNotLoggedInSince(@Param("date") LocalDate date);
 }
